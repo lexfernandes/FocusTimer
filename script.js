ffc4aadfc573ff61abda1fbd384a34ba1173d441
lexfernandes <lexlopesfernandes@gmail.com>
2023-09-10 20:28:41 -0300

    let  por const

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let hours = document.querySelector(".hours")
 let minutes = document.querySelector(".minutes")
 
-let play = document.querySelector(".play")
-let set = document.querySelector(".set")
-let pause = document.querySelector(".pause")
-let stop = document.querySelector(".stop")
+const play = document.querySelector(".play")
+const set = document.querySelector(".set")
+const pause = document.querySelector(".pause")
+const stop = document.querySelector(".stop")
 
 let soundON = document.querySelector(".sound-on")
 let soundOff = document.querySelector(".sound-off")
