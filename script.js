e96f793ba54ba1107fac317475062d45532a0a10
lexfernandes <lexlopesfernandes@gmail.com>
2023-09-11 11:48:56 -0300

    update 1.1

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const minutesDisplay = document.querySelector(".minutes")
+//-------------------VARIAVEIS--------------------------//
 let secondsDisplay = document.querySelector(".seconds")
-let minutes
+const minutesDisplay = document.querySelector(".minutes")
+let minutes = Number(minutesDisplay.textContent)
+let TimerTimeOut
 
 const play = document.querySelector(".play")
 const set = document.querySelector(".set")
@@ -9,18 +11,7 @@ const stop = document.querySelector(".stop")
 
 let soundON = document.querySelector(".sound-on")
 let soundOff = document.querySelector(".sound-off")
-//----------------------------------------------------------------------------------------------//
-
-function countDown() {
-  setTimeout(function () {
-    let seconds = Number(secondsDisplay.textContent)
-    if (seconds <= 0) {
-      seconds = 60
-    }
-    secondsDisplay.textContent = seconds - 1
-    countDown()
-  }, 1000)
-}
+//--------------------------EVENTOS------------------------------------------//
 play.addEventListener("click", function () {
   play.classList.add("hide")
   pause.classList.remove("hide")
@@ -28,24 +19,66 @@ play.addEventListener("click", function () {
   stop.classList.remove("hide")
   countDown()
 })
+
 pause.addEventListener("click", function () {
-  play.classList.remove("hide")
   pause.classList.add("hide")
+  play.classList.remove("hide")
+  clearTimeout(TimerTimeOut)
 })
+
 stop.addEventListener("click", function () {
-  set.classList.remove("hide")
-  stop.classList.add("hide")
-  pause.classList.add("hide")
-  play.classList.remove("hide")
+  resetControls()
+  resetTimer()
 })
 
 set.addEventListener("click", function () {
-  minutes = prompt("Digite a Hora")
-  seconds = prompt("Digite o Segundos")
-  minutesDisplay.textContent = minutes
-  secondsDisplay.textContent = seconds
+  let newMinutes = prompt("Digite os minutos")
+  if (!newMinutes) {
+    resetTimer()
+    return
+  }
+  minutes = newMinutes
+  updateTimerDisplay(minutes, 0)
 })
+//--------------------------------------FUNÇÕES-----------------------------------------------//
+function updateTimerDisplay(minutes, seconds) {
+  minutesDisplay.textContent = String(minutes).padStart(2, "0")
+  secondsDisplay.textContent = String(seconds).padStart(2, "0")
+}
 
+function resetTimer() {
+  updateTimerDisplay(minutes, 0)
+  clearTimeout(lTimerTimeOut)
+}
+
+function resetControls() {
+  play.classList.remove("hide")
+  pause.classList.add("hide")
+  set.classList.remove("hide")
+  stop.classList.add("hide")
+}
+
+function countDown() {
+  TimerTimeOut = setTimeout(function () {
+    let seconds = Number(secondsDisplay.textContent)
+    let minutes = Number(minutesDisplay.textContent)
+
+    updateTimerDisplay(minutes, 0)
+
+    if (minutes <= 0) {
+      resetControls()
+      return
+    }
+    if (seconds <= 0) {
+      seconds = 60
+      --minutes
+    }
+
+    updateTimerDisplay(minutes, String(seconds - 1))
+    countDown()
+  }, 1000)
+}
+//----------------------------------------------------------------------------------------------//
 soundON.addEventListener("click", function () {
   soundON.classList.add("hide")
   soundOff.classList.remove("hide")
